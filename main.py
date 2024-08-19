5287a0cc54ef3838e3bc7909f1357153d0b314bc
ivalukyan <ivalukyanov@gmail.com>
2024-08-20 00:43:00 +0300

    add new template

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import uvicorn
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, APIRouter, Form
 from starlette.templating import Jinja2Templates
+from typing import Annotated
 
 app = FastAPI(
     title='Yclients'
 )
 
+router = APIRouter(
+    tags=['record']
+)
+
 templates = Jinja2Templates(directory="app/templates")
 
+data = {}
 
-@app.get("/")
+
+@router.get("/")
 async def home(request: Request):
-    return templates.TemplateResponse("recording/home.html", {'request': request})
+    return templates.TemplateResponse("index.html", {'request': request})
+
+
+@router.get("/records")
+async def recording(request: Request):
+    return templates.TemplateResponse("recording/record.html", {'request': request})
+
+
+@router.post("/records")
+async def recording(request: Request, record_select: Annotated[str, Form()], date: Annotated[str, Form()],
+                    human_select: Annotated[str, Form()], name: Annotated[str, Form()], phone: Annotated[str, Form()],
+                    email: Annotated[str, Form()] | None = "", comment: Annotated[str, Form()] | None = ""):
+    return templates.TemplateResponse("recording/record_answer.html", {'request': request})
+
+
+app.include_router(router)
 
 
 @app.exception_handler(404)
@@ -32,3 +54,7 @@ async def custom_400_handler(request: Request, exp: HTTPException):
 @app.exception_handler(405)
 async def custom_405_handler(request: Request, exp: HTTPException):
     return templates.TemplateResponse("exception_handler/405.html", {"request": request, 'exception': exp})
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
