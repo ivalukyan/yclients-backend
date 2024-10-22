ee96f8555fcb9984723dd0ca738963ff6b85ef6d
ivalukyan <ivalukyanov@gmail.com>
2024-10-22 14:54:34 +0300

    fix

--- app/routers/services.py
+++ app/routers/services.py
@@ -103,7 +103,7 @@ async def get_reserve_times(time: Times):
     times = await api.book_times(staff_id=time.staff_id, date=time.select_date)
     # print(times)
     available_times = await get_times(times.values())
-    # print(available_times)
+    print(available_times)
     content = f"{time.staff_id} -- {time.select_date}"
 
     return {'available_times': available_times,
