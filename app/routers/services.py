b6e82e440e5983dfb7eb65eb5f3e006296a5870c
iva <ivalulyanov1@gmail.com>
2024-11-06 15:39:32 +0300

    Feat

--- app/routers/services.py
+++ app/routers/services.py
@@ -54,7 +54,8 @@ async def category_services(request: Request):
     category = await api.book_category()
     print(category)
 
-    return templates.TemplateResponse("booking/category_services.html", {'request': request, 'category': category})
+    return templates.TemplateResponse("booking/category_services.html", {'request': request,
+                                                                         'category': category.values()})
 
 
 
