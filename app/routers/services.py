655b8cfae7815451521bd97d68ffdc565374e7a4
iva <ivalulyanov1@gmail.com>
2024-11-30 13:40:32 +0300

    Fix

--- app/routers/services.py
+++ app/routers/services.py
@@ -49,6 +49,7 @@ async def book_services(request: Request):
 
     services = await api.book_services()
     values = list(services.values())
+    print(values)
 
     for i in values:
         i['service_description'] = await remove_html_tags(i['service_description'])
